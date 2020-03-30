54fa2a8416a1e74c94edb529ac1b5fa96e20564d
yhkl-dev <kaiyang939325@gmail.com>
2020-03-30 21:58:07 +0800

    update

--- yhkl/lisp/fibo_prac.py
+++ yhkl/lisp/fibo_prac.py
@@ -9,9 +9,16 @@ def fib_iter(a, b, c, count):
     return fib_iter(a + 2*b + 3*c, a, b, count - 1)
 
 
+def f(n):
+    if n >= 3:
+        return f(n-1) + 2*f(n-2) + 3 * f(n-3)
+    return n
+
+
+
 if __name__ == "__main__":
     print(fib(4))
     print(fib(5))
-    print(fib(6))
-
+    print("-" * 100)
+    print(fib(100))
 
