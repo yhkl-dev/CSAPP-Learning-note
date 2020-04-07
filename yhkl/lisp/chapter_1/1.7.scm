6f8a017d4525795f7179901d0556dc85684bc48c
yhkl-dev <kaiyang939325@gmail.com>
2020-04-07 22:10:41 +0800

    add 1.7.scm

--- yhkl/lisp/chapter_1/1.7.scm
+++ yhkl/lisp/chapter_1/1.7.scm
@@ -1,23 +1,24 @@
-(define (sqrt x)
-	(sqrt-iter 1.0 x 0.0001))
-
-(define (sqrt-iter guess x change)
-	(if (good-enough? guess x change)
-			guess
-			(sqrt-iter (improve guess x)
-								x
-								(- guess change))))
-
-(define (improve guess x)
-	(average (guess (/ x guess))))
-
 (define (average x y)
 	(/ (+ x y) 2))
 
-(define (good-enough? guess x change)
-	())
+(define (abs x)
+	(cond ((< x 0) (- x))
+				(else x)))
 
 (define (square x)
 	(* x x))
 
+(define (sqrt-iter guess x)
+	(if (new-good-enough? guess (improve guess))
+			guess
+			(sqrt-iter (improve guess x))))
 
+(define (improve guess x)
+	(average guess (/ x guess)))
+
+(define (new-good-enough? guess new-guess)
+	(< (abs (/ (- guess new-guess) guess)) 
+		0.001))
+
+(define (sqrt x)
+	(sqrt-iter 1.0 x))
