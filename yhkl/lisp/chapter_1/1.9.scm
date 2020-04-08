fbcea57d58425d285633afc764f672b45e9eb664
yhkl-dev <kaiyang939325@gmail.com>
2020-04-08 21:30:01 +0800

    update

---
+++ yhkl/lisp/chapter_1/1.9.scm
@@ -0,0 +1,15 @@
+(define (+ b c)
+	(if (= a 0)
+			b
+			(inc (+ dec a) b)))
+
+(define (+ a b)
+	(if (= a 0)
+			b
+			(+ (dec a) (inc b))))
+
+(define (inc x)
+	(+ x 1))
+
+(define (dec x)
+	(- x 1))
