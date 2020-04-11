59e5193669548f4e8ace5f4788760214698c05e8
yhkl-dev <kaiyang939325@gmail.com>
2020-04-11 17:02:20 +0800

    update

--- yhkl/lisp/chapter_1/1.13.scm
+++ yhkl/lisp/chapter_1/1.13.scm
@@ -1,3 +1,9 @@
+(define (new-fib n)
+	(cond ((= n 0) 0)
+				((= n 1) 1)
+				(else (+ (new-fib (- n 1))
+								 (new-fib (- n 2))))))
+
 (define (fib n)
 	(fib-iter 1 0 n))
 
@@ -8,12 +14,27 @@
 
 (define fi (/ (+ 1 (sqrt 5)) 2))
 
-(define (goog-enough? fib fi)
-	(< (abs (- fib fi)) 0))
+(define (good-enough? n)
+	(< (abs (- (fib n) (/ (fin fi n) (sqrt 5)))) 0.0001))
 
 (define (abs x)
 	(if (< x 0) 
 			(- x)
 			x))
 
+(define (fin fi n)
+	(if (= n 0) 
+			1
+			(* fi (fin fi (- n 1)))))
 
+(define (expt fi n)
+	(expt-iter fi n 1)
+	)
+
+(define (expt-iter fi counter product)
+	(if (= counter 0)
+			product
+			(expt-iter fi
+								 (- counter 1)
+								 (* fi product))))
+						
