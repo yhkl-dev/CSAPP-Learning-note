8c2fce2834400e928dc92677e802d95584c61372
yhkl-dev <kaiyang939325@gmail.com>
2020-04-21 22:25:33 +0800

    update clue binary tree

--- yhkl/data_structure/tree/clue_binary_tree.c
+++ yhkl/data_structure/tree/clue_binary_tree.c
@@ -34,3 +34,42 @@ void inTreading(biThrTree p) {
 		inTreading(p->right);
 	}
 }
+
+bool inOrderTraverseTree(biThrTree T) {
+	biThrTree p;
+	p = T->left;
+	while (p != T) {
+		while (p->leftTag == Link) {
+			p = p->left;
+		}
+		printf("%c", p->data);
+		while (p->rightTag == Thread && p->right != T) {
+			p = p->right;
+			printf("%c", p->data);
+		}
+		p = p->left;
+	}
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
