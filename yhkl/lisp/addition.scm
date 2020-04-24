fd9c08c831c57bfaeb340a7cef05965debe7f3c7
yhkl-dev <kaiyang939325@gmail.com>
2020-04-24 22:47:34 +0800

    update

--- yhkl/lisp/addition.scm
+++ yhkl/lisp/addition.scm
@@ -1,4 +1,9 @@
-(define (+ x y)
+;(define (+ x y)
+;	(if (= x 0)
+;			y
+;			(+ (- x 1) (+ 1 y))))
+
+(define (plus x y)
 	(if (= x 0)
 			y
-			(+ (- x 1) (+ 1 y))))
+			(plus 1 (plus (- x 1) y))))
