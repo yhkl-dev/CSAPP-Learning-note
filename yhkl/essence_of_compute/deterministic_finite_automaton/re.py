114270ec6dde87698265148aa2d05284b578949e
yhkl-dev <kaiyang939325@gmail.com>
2020-05-01 23:55:30 +0800

    update

---
+++ yhkl/essence_of_compute/deterministic_finite_automaton/re.py
@@ -0,0 +1,43 @@
+class Pattern(object):
+
+    def __init__(self, precedence):
+        self.precedence = precedence
+
+    def bracket(outer_precedence):
+        if self.precedence < outer_precedence:
+            return "({})".format(self.to_s())
+
+
+class Empty(Pattern):
+
+    def __init__(self):
+        self.precedence = 3
+
+    def to_s(self):
+        return ''
+
+
+class Literal(Pattern):
+
+    def __init__(self, character):
+        super(Pattern, self).__init__()
+        self.character = character
+        self.precedence = 3
+
+    def to_s(self):
+        return self.character
+
+
+class Concatenate(Pattern):
+
+    def __init__(self, first, second):
+        super(Pattern, self).__init__()
+        self.precedence = 3
+        self.first = first
+        self.second = second
+
+    def to_s(self):
+        return [self.first, self.second]
+
+
+
