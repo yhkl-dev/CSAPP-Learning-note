d39353277c1d803352352f17ab7f4c53bcefbfaf
yhkl-dev <kaiyang939325@gmail.com>
2020-05-11 22:26:40 +0800

    update

--- essence_of_compute/pushdown_automaton/pda.py
+++ essence_of_compute/pushdown_automaton/pda.py
@@ -25,20 +25,75 @@ class PDARule(object):
                 self.character == character
 
     def follow(self, configuration):
-        return PDAConfiguration(self.next_state, slef.next_stack(configuration))
+        print('2')
+        return PDAConfiguration(self.next_state, self.next_stack(configuration))
 
     def next_stack(self, configuration):
 
         poped_stack = configuration.stack.pop()
-        for s in self.push_character.reverse():
+        self.push_character.reverse()
+        for s in self.push_character:
             poped_stack = poped_stack.push(s)
 
         return poped_stack
 
-    
+
+class DPDARulebook(object):
+
+    def __init__(self, rules):
+        self.rules = rules
+
+    def next_configuration(self, configuration, character):
+        rule = self.rule_for(configuration, character)
+        print('rule', rule.__dict__)
+        print(type(rule))
+        return rule.follow(configuration)
+
+    def rule_for(self, configuration, character):
+        for rule in self.rules:
+            if rule.applies_to(configuration, character):
+                print('1')
+                print(rule)
+                return rule 
+
+
+class DPDA(object):
+
+    def __init__(self, current_configuration, accept_states, rulebook):
+        self.current_configuration = current_configuration
+        self.accept_states = accept_states
+        self.rulebook = rulebook
+
+    def accepting(self):
+        return self.current_configuration.state in self.accept_states
+
+    def read_character(self, character):
+        self.current_configuration = \
+                self.rulebook.next_configuration(self.current_configuration, character)
+
+    def read_string(self, string):
+        for s in string:
+            self.read_character(s)
 
 if __name__ == '__main__':
     rule = PDARule(1, '(', 2, '$', ['b', '$'])
     configuration = PDAConfiguration(1, Stack(['$']))
     print(rule.applies_to(configuration, '('))
+    
+    print('-' * 100)
 
+    rulebook = DPDARulebook([
+        PDARule(1, '(', 2, '$', ['b', '$']),
+        PDARule(2, '(', 2, 'b', ['b', 'b']),
+        PDARule(2, ')', 2, 'b', []),
+        PDARule(2, None, 1, '$', ['$']),
+    ])
+    configuration = rulebook.next_configuration(configuration, '(')
+    print(configuration)
+
+    print('-' * 100)
+    dpda = DPDA(PDAConfiguration(1, Stack(['$'])), [1], rulebook)
+    print(dpda.accepting())
+    dpda.read_string("(()")
+    print(dpda.accepting())
+    
