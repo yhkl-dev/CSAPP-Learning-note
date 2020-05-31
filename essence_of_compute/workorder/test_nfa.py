38a2eed6ffdf78741c9955e6d619b615270f9909
yhkl-dev <kaiyang939325@gmail.com>
2020-05-31 22:42:44 +0800

    update

--- essence_of_compute/workorder/test_nfa.py
+++ essence_of_compute/workorder/test_nfa.py
@@ -5,7 +5,9 @@ class TestNFA(unitest.TestCase):
 
     def setUp(self):
         self.rulebook = DFARulebook(
-            [
                 FARule(),
             ]
         )
+
+    def test_rulebook(self):
+        print(self.rulebook)
