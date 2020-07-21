d010e8beb26565bbef629b8fc87b96c85df6633f
yhkl-dev <kaiyang939325@gmail.com>
2020-07-21 23:06:03 +0800

    NO MESSAGE COMMIT

--- golang/flag/main.go
+++ golang/flag/main.go
@@ -6,7 +6,17 @@ import (
 )
 
 func main() {
-	var file = flag.String("f", "/root/.vimrc", "the file name")
+	//	var file = flag.String("f", "/root/.vimrc", "the file name")
+	var fileName string
+	flag.StringVar(&fileName, "f", "/root/.vimrc", "the file name new")
+	var newLineMark bool
+
+	flag.BoolVar(&newLineMark, "m", false, "是否换行")
+
 	flag.Parse()
-	fmt.Println("the file name is: ", *file)
+	//	fmt.Println("the file name is: ", *file)
+	if newLineMark {
+		fmt.Println()
+	}
+	fmt.Println("the file name is: ", fileName)
 }
