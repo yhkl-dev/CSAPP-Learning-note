332439753d17d58832e5107f86e0649d2c00a635
yhkl-dev <kaiyang939325@gmail.com>
2020-08-08 23:48:53 +0800

    NO MESSAGE COMMIT

--- python/fluent_python/decorator/base.py
+++ python/fluent_python/decorator/base.py
@@ -4,3 +4,59 @@
     函数或可调用对象
 
 '''
+
+
+def deco(func):
+    def inner():
+        print("running inner()")
+    return inner
+
+
+@deco
+def target():
+    print("running target()")
+
+
+target()
+
+'''
+    Python 何时执行装饰器
+
+    装饰器的一个关键特性就是，他们再被装饰的函数定义后立即执行
+    这通常是在模块加载时
+
+'''
+
+registry_list = []
+
+
+def registry(func):
+    print('running register(%s)' % func)
+    registry_list.append(func)
+    return func
+
+
+@registry
+def f1():
+    print('running f1()')
+
+
+@registry
+def f2():
+    print('running f2()')
+
+
+def f3():
+    print('running f3()')
+
+
+def main():
+    print("running main()")
+    print('registry ->', registry_list)
+    f1()
+    f2()
+    f3()
+
+
+if __name__ == "__main__":
+    main()
