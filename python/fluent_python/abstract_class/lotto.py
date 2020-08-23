328e9a348b23164e9627e6670d7f37d886f51969
yhkl-dev <kaiyang939325@gmail.com>
2020-08-23 21:37:43 +0800

    NO MESSAGE COMMIT

---
+++ python/fluent_python/abstract_class/lotto.py
@@ -0,0 +1,25 @@
+import random
+
+from tombola import Tombola
+
+
+class LotteryBlower(Tombola):
+
+    def __init__(self, iterable):
+        self._balls.extend(iterable)
+
+    def load(self, iterable):
+        self._balls.extend(iterable)
+
+    def pick(self):
+        try:
+            position = random.randrange(len(self._balls))
+        except ValueError:
+            raise LookUpError("pick from empty LotteryBlower")
+        return self._balls.pop(position)
+
+    def loaded(self):
+        return bool(self._balls)
+
+    def inspect(self):
+        return tuple(sorted(self._balls))
