df2b251e033fd54ec15661c093ebbb1ac2e6386c
yhkl-dev <kaiyang939325@gmail.com>
2021-05-23 16:29:42 +0800

    map2struct

--- golang/go_base/chapter_1/goplus/src/main.go
+++ golang/go_base/chapter_1/goplus/src/main.go
@@ -10,6 +10,32 @@ type User struct {
 	UserName string
 }
 
+func Map2Struct(m map[string]interface{}, u interface{}) {
+	v := reflect.ValueOf(u)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+		if v.Kind() != reflect.Struct {
+			panic("must be struct")
+		}
+		findFromMap := func(key string) interface{} {
+			for k, v := range m {
+				if k == key {
+					return v
+				}
+			}
+			return nil
+		}
+		for i := 0; i < v.NumField(); i++ {
+			getValue := findFromMap(v.Type().Field(i).Name)
+			if getValue != nil && reflect.ValueOf(getValue).Kind() == v.Field(i).Kind() {
+				v.Field(i).Set(reflect.ValueOf(getValue))
+			}
+		}
+	} else {
+		panic("must be pointer")
+	}
+}
+
 func main() {
 	// // u := User{1, "yhkl"}
 	// u := User{}
@@ -57,4 +83,13 @@ func main() {
 	}
 	fmt.Println(u)
 
+	x := &User{}
+	m := map[string]interface{}{
+		"Id":       123,
+		"UserId":   101,
+		"UserName": "yhkl",
+		"age":      100,
+	}
+	Map2Struct(m, x)
+	fmt.Println(x)
 }
